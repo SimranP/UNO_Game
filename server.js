99887701204f3983a81e8dcc0ff3ddd69da6796c
Madhuri Patil <madhuri@thoughtworks.com>
2016-01-19 15:19:33 +0530

    added express new version to work with

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 var http  = require('http');
-// var express = require('express'); 
+var express = require('express'); 
 
 req_handeller = function(req,res){
-    res.end('hello world');
+    if(req.url == '/')
+    res.redirect('./index.html');
 };
 
 var server = http.createServer(req_handeller);
 server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080,process.env.OPENSHIFT_NODEJS_IP||'127.0.0.1');
 
-
-
-// // var zapp = new req_handeller();
-// // zapp.initialize();
-// zapp.start();
