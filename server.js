0df717e50492ce107d39ebaeed17c398c4c24f06
Madhuri Patil <madhuri@thoughtworks.com>
2016-01-19 15:10:37 +0530

    changed

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var http  = require('http');
-var express = require('express'); 
+// var express = require('express'); 
 
 req_handeller = function(req,res){
-    if(req.url == '/')
-        res.end('./index.html');
+    res.end('hello world');
 };
 
-var server = http.createServer(function(req.res){
-    req_handeller(req,res);
-});
-server.listen(9000);
+var server = http.createServer(req_handeller);
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080,process.env.OPENSHIFT_NODEJS_IP||'127.0.0.1');
 
+
+
+// // var zapp = new req_handeller();
+// // zapp.initialize();
+// zapp.start();
